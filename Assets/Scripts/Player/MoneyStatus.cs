88abcca791042cb0e405eda2d37a355e7efffbc8
Michal Louda <mloud@Michals-MacBook-Pro.local>
2015-02-09 21:57:00 +0100

    update

---
+++ Assets/Scripts/Player/MoneyStatus.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleJSON;
+
+
+
+namespace GameStatus
+{
+
+    public class MoneyStatus
+    {
+
+		public int Pogs { get { return pogs; } }
+
+        private int pogs;
+
+
+		public MoneyStatus()
+        {
+			pogs = 100;// inital pogs
+		}
+
+        public void Reset()
+        {
+            UnityEngine.PlayerPrefs.DeleteKey("MoneyStatus");
+        }
+
+
+		public void AddPogs(int count)
+		{
+			pogs += count;
+		}
+
+		public bool UsePogs(int count)
+		{
+			if (pogs >= count)
+			{
+				pogs -= count;
+				return true;
+			}
+
+			return false;
+		}
+
+
+        public JSONNode GetAsJSON()
+        {
+            JSONClass root = new JSONClass();
+
+            // last level reached
+            root["saveVersion"] = Versions.SaveData;
+            root["pogs"].AsInt = Pogs;
+
+            Core.Dbg.Log("MoneyStatus.GetAsJson() " + root.ToString());
+
+            return root;
+        }
+
+        public void Load(JSONNode jsonNode, bool allowMerge)
+        {
+            if (jsonNode == null)
+            {
+                Core.Dbg.Log("MoneyStatus.Load() jsoNode is null -> no MoneyStatus found", Core.Dbg.MessageType.Warning);
+                return; 
+            }
+
+            string version = jsonNode["saveVersion"].Value;
+
+			if (allowMerge)
+			{
+				pogs = Math.Max(pogs, jsonNode["pogs"].AsInt);
+			}
+			else
+			{
+				pogs = jsonNode["pogs"].AsInt;
+			}
+        }
+
+    }
+}
+
+   
+     
--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -8,25 +8,35 @@ namespace GameStatus
 
     public class PlayerStatus : MonoBehaviour
     {
+
+		public MoneyStatus MoneyStatus {get; private set; }
         public LevelsStatus LevelsStatus { get; private set; }
         public SpecialAbilitiesStatus AbilitiesStatus { get; private set; }
 
-      
+
+
         public void Init()
         {
+			MoneyStatus = new MoneyStatus();
             LevelsStatus = new LevelsStatus();
             AbilitiesStatus = new SpecialAbilitiesStatus();
         }
 
         public string GetSaveGame()
         {
+
+			SimpleJSON.JSONNode moneyJson = MoneyStatus.GetAsJSON();
             SimpleJSON.JSONNode levelsJson = LevelsStatus.GetAsJSON();
-            SimpleJSON.JSONNode abilitiesJson = AbilitiesStatus.GetAsJSON();
+			SimpleJSON.JSONNode abilitiesJson = AbilitiesStatus.GetAsJSON();
+
 
             SimpleJSON.JSONClass root = new SimpleJSON.JSONClass();
 
-            root["levelsStatus"] = levelsJson;
+            
+			root["moneyStatus"] = moneyJson;
+			root["levelsStatus"] = levelsJson;
             root["abilitiesStatus"] = abilitiesJson;
+
 
 
             return root.ToString();
@@ -44,6 +54,7 @@ namespace GameStatus
 
             if (jsonRoot != null)
             {
+				MoneyStatus.Load(jsonRoot["moneyStatus"], allowMerge);
                 LevelsStatus.Load(jsonRoot["levelsStatus"], allowMerge);
                 AbilitiesStatus.Load(jsonRoot["abilitiesStatus"], allowMerge);
             }
